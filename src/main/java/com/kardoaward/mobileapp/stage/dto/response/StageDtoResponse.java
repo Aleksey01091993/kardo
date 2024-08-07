d94ec7a6889076f843924306f486eb2d3e230d3c
79686 <mrak5455@yandex.ru>
2024-08-07 17:42:55 +0300

    новый комит

--- src/main/java/com/kardoaward/mobileapp/stage/dto/response/StageDtoResponse.java
+++ src/main/java/com/kardoaward/mobileapp/stage/dto/response/StageDtoResponse.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 
+
 @Schema(description = "Сущность для отображения этапа.")
 public record StageDtoResponse(
         Long id,
