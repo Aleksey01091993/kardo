cd8f2400dac523b5865dda3fca1e37b1171d0bbe
NikitaLevshin <levshinno@yandex.ru>
2024-08-12 14:16:20 +0300

    поддержка всех видео

--- src/main/java/com/kardoaward/mobileapp/video/controller/VideoController.java
+++ src/main/java/com/kardoaward/mobileapp/video/controller/VideoController.java
@@ -31,7 +31,7 @@ public class VideoController {
 
             if (file.exists() || file.isReadable()) {
                 return ResponseEntity.ok()
-                        .contentType(MediaType.parseMediaType("video/quicktime"))
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
                         .header(HttpHeaders.CONTENT_DISPOSITION,
                                 "inline; filename=\"" + file.getFilename() + "\"")
                         .body(file);
